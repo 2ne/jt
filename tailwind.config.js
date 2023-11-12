a984a6ea53f3a5c4208628dda67a8f1817087fc5
James <jamestoone@gmail.com>
2023-11-12 10:48:47 +0000

    fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   content: ['./dist/**/*.html', './src/**/*.{js,jsx,ts,tsx}', './*.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  }
 }
