96b453af7129047b185a68551bd2b759bd8ada5e
James <jamestoone@gmail.com>
2023-11-12 13:09:59 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,19 @@ import React, { ReactElement } from 'react'
 
 function App(): ReactElement {
   return (
-    <div className="min-h-screen bg-zinc-950 text-zinc-50">
-      <div className="overflow-hidden bg-[url('./assets/2.png')] bg-fixed bg-no-repeat bg-[center_top_-8rem] absolute inset-6 rounded-3xl" />
-      <header className="w-full p-6 mx-auto">
+    <div className="min-h-[500vh] bg-zinc-950 text-zinc-50">
+      <div className="grayscale brightness-[25%] overflow-hidden bg-[url('./assets/2.png')] bg-fixed bg-no-repeat absolute inset-0 bg-cover" />
+      <header className="sticky top-0 w-full p-6 mx-auto">
         <div className="flex items-center">
           <h1 className="text-xl font-semibold line">James Toone</h1>
         </div>
       </header>
-      <section className="grid place-content-center w-full min-h-[75vh] p-6 mx-auto text-center">
-        <div className="space-y-5">
-          <h2 className="text-6xl font-semibold line-lg">
+      <section className="position relative grid place-content-center w-full min-h-[calc(100vh-9rem)] p-6 mx-auto text-center">
+        <div className="space-y-8">
+          <h2 className="font-bold text-6xl line-lg drop-shadow-[0_0_12px_rgba(9,9,1,1)]">
             Lead Product Designer
           </h2>
-          <p className="text-xl font-semibold text-zinc-500 text-balance max-w-[48ch]">
+          <p className="text-xl font-semibold text-zinc-500 text-balance w-full mx-auto max-w-[48ch] drop-shadow-[0_0_12px_rgba(9,9,1,1)]">
             Creating visually stunning & intuitive designs that provide
             exceptional user experiences.
           </p>
