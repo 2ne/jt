3e068f685fb182ea597158ab4383e8593930f1cf
James <jamestoone@gmail.com>
2023-11-13 16:35:45 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import useMousePosition from './useMousePosition'
 import { motion } from 'framer-motion'
 
 function App(): ReactElement {
+  const [isSidebar, setIsSidebar] = useState(false)
   const [isHovered, setIsHovered] = useState(false)
   const ref = useRef(null)
   const { x, y } = useMousePosition(ref)
@@ -40,22 +41,37 @@ function App(): ReactElement {
             transition={{ type: 'tween', ease: 'backOut', duration: 0.5 }}
           ></motion.div>
         </div>
-        <aside className="fixed inset-0 right-auto p-6 pt-[5.5rem] overflow-auto border-r shadow-lg shadow-zinc-950 border-zinc-500/10 bg-neutral-900/75 backdrop-blur-lg w-80">
+        <aside
+          className={`fixed inset-0 right-auto p-4 pt-[5.5rem] overflow-auto border-r shadow-lg shadow-zinc-950 border-zinc-500/10 bg-neutral-900/75 backdrop-blur-lg w-80 transition-transform duration-300 ${
+            isSidebar
+              ? 'transform translate-x-0'
+              : 'transform -translate-x-full'
+          }`}
+        >
           <div className="border-t border-b border-t-zinc-500/25 border-b-zinc-950"></div>
         </aside>
         <header className="fixed top-0 z-10 w-full p-6 mx-auto">
           <div className="flex items-center gap-3.5">
             <button
               type="button"
+              onClick={() => {
+                setIsSidebar((prevIsSidebar) => !prevIsSidebar)
+              }}
               className="relative group text-left cursor-pointer flex items-center gap-3.5 -m-3 p-3"
             >
-              <div className="absolute inset-0 transition-opacity duration-500 rounded-md opacity-0 pointer-events-none group-hover:opacity-100 bg-zinc-700/50 backdrop-blur-md backdrop-saturate-150 backdrop-brightness-150"></div>
+              <div
+                className={`absolute inset-0 transition-opacity duration-500 pointer-events-none group-hover:opacity-100 bg-zinc-700/50 backdrop-blur-md backdrop-saturate-150 backdrop-brightness-150 ${
+                  isSidebar
+                    ? ' left-auto h-10 w-10 top-3 opacity-50 right-2.5 rounded-full '
+                    : ' rounded-lg opacity-0 '
+                }`}
+              ></div>
               <div className="relative">
                 <img
-                  className="w-10 h-10 rounded-full ring-2 ring-zinc-950/75"
+                  className="w-10 h-10 rounded-full ring-1 ring-zinc-950/75"
                   src="./assets/me.jpeg"
                 />
-                <span className="absolute w-2.5 h-2.5 rounded-full bg-emerald-500 ring-2 ring-zinc-950/75 -top-0.5 -left-0.5 animate-pulse"></span>
+                <span className="absolute w-2.5 h-2.5 rounded-full bg-emerald-500 ring-1 ring-zinc-950/75 -top-0.5 -left-0.5 animate-pulse"></span>
               </div>
               <div className="relative flex items-center gap-3.5 text-sm">
                 <div>
@@ -72,7 +88,30 @@ function App(): ReactElement {
                     height="24"
                     fill="none"
                     viewBox="0 0 24 24"
-                    className="w-8 h-8 -ml-8 text-transparent transition-all duration-500 group-hover:text-current group-hover:ml-0"
+                    className={`w-8 h-8 text-transparent transition-all duration-500 group-hover:text-current  ${
+                      isSidebar
+                        ? 'rotate-180 ml-6 text-current'
+                        : '-ml-8 text-transparent group-hover:ml-0'
+                    }`}
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="1.5"
+                      d="M10.75 8.75L14.25 12L10.75 15.25"
+                    ></path>
+                  </svg>
+                  <svg
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className={`w-8 h-8 absolute top-1 right-[5px] text-current transition-all duration-500  ${
+                      isSidebar
+                        ? 'rotate-0 opacity-100'
+                        : '-rotate-180 opacity-0'
+                    }`}
                   >
                     <path
                       stroke="currentColor"
