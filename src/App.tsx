02355bcad121ce676a32b8df16e343c57048bd76
James <jamestoone@gmail.com>
2023-11-14 08:34:43 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -155,7 +155,7 @@ function App(): ReactElement {
         </header>
         <section className="relative grid w-full min-h-screen p-6 mx-auto text-center pointer-events-none position place-content-center">
           <div className="space-y-2">
-            <h2 className="sm:whitespace-nowrap max-sm:pb-6 max-sm:line-through max-sm:decoration-[3px] max-sm:decoration-zinc-950 sm:leading-normal font-bold text-6xl sm:line drop-shadow-[0_0_12px_rgba(9,9,1,1)] text-transparent bg-clip-text bg-gradient-to-b from-white via-white to-zinc-600">
+            <h2 className="tracking-[-0.5px] sm:whitespace-nowrap max-sm:pb-6 max-sm:line-through max-sm:decoration-[3px] max-sm:decoration-zinc-950 sm:leading-normal font-bold text-6xl sm:line drop-shadow-[0_0_12px_rgba(9,9,1,1)] text-transparent bg-clip-text bg-gradient-to-b from-white via-white to-zinc-600">
               Lead Product Designer
             </h2>
             <p className="text-xl font-semibold text-zinc-400/90 text-balance w-full mx-auto max-w-[48ch] drop-shadow-[0_0_12px_rgba(9,9,1,1)]">
